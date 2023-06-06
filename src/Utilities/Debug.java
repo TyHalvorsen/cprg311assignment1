de07e83ecf8583f6a2a24d6bd3b5c14397914f64
Lynn+Marcel+Tyler <>
2023-06-06 17:36:52 -0600

    update Debug / Sorting / AppDriver

---
+++ src/Utilities/Debug.java
@@ -0,0 +1,95 @@
+package utilities;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * 
+ */
+public class Debug {
+    
+private int debugLevel;
+
+	public Debug(int selectedDebugLevel) {
+
+		this.debugLevel = selectedDebugLevel;
+
+	}
+
+	// Getter and Setter Methods
+	/**
+	 * @return the current debug level
+	 */
+	public int getLevel() {
+		return this.debugLevel;
+	}
+
+	/**
+	 * @param debugLevel - the current debug level
+	 */
+	public void setLevel(int selectedDebugLevel) {
+
+		if (selectedDebugLevel > 0) {
+
+			this.debugLevel = selectedDebugLevel;
+		}
+
+	}
+
+	// Operational Methods
+	/**
+	 * @param msg - the debug output message
+	 */
+	public void output(int selectedDebugLevel, String msg) {
+
+		if (selectedDebugLevel <= this.debugLevel) {
+
+			System.out.println(msg); // message to be displayed
+		}
+
+		/* Else debug == 0 , do nothing */
+
+	}
+
+	/**
+	 * 
+	 * @param selectedDebugLevel - entered app level
+	 * @param msg                - msg to display
+	 * @param answer             - yes or no to serialize dbug output to file
+	 */
+	public void output(int selectedDebugLevel, String msg, String answer) {
+
+		int num = 1;
+
+		if (selectedDebugLevel <= this.debugLevel && answer.toUpperCase().equals("YES")) {
+
+			String fileName = "debugResults" + num + "debugLevel" + selectedDebugLevel;
+
+			System.out.println(msg); // message to be displayed
+
+			try {
+				FileOutputStream fos = new FileOutputStream("res/debugOutput/" + fileName);
+
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+				oos.writeObject(msg);
+
+				// close object first
+				oos.close();
+				// close file
+				fos.close();
+
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		/* Else debug == 0 , do nothing */
+
+		num++;
+	}
+
+}
--- src/sortingoutsorting/AppDriver.java
+++ src/sortingoutsorting/AppDriver.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sortingoutsorting;
+package driver;
+
+
 
 import Comparator.BaseAreaComparator;
 import Comparator.VolumeComparator;
@@ -11,11 +8,18 @@ import Shape.Cone;
 import Shape.Cylinder;
 import Shape.Pyramid;
 import Shape.Shape;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import utilities.Debug;
+import utilities.Sorting;
 
-
+/**
+ *
+ * 
+ */
 public class AppDriver {
 
     /**
@@ -25,7 +29,7 @@ public class AppDriver {
 
         BaseAreaComparator baseComp = new BaseAreaComparator();
         VolumeComparator volumeComp = new VolumeComparator();
-        
+
         Cone cone = new Cone(28437.601, 17222.616);
         Cylinder cylinder = new Cylinder(12595.147, 650.338);
         Pyramid pyramid = new Pyramid(13081.024, 12232.336);
@@ -39,28 +43,59 @@ public class AppDriver {
 
         System.out.println(cone.compareTo(cylinder));
 
-        ArrayList<Shape> ShapeList = new ArrayList<>();
-        ShapeList.add(cone);
-        ShapeList.add(cylinder);
-        ShapeList.add(pyramid);
+        ArrayList<Shape> shapeList = new ArrayList<>();
+        shapeList.add(cone);
+        shapeList.add(cylinder);
+        shapeList.add(pyramid);
 
         System.out.println("cone base area " + cone.getBaseArea());
         System.out.println("cylinder base area " + cylinder.getBaseArea());
         System.out.println("pyramid base area " + pyramid.getBaseArea());
+
         System.out.println("cone getVolume " + cone.getVolume());
         System.out.println("cylinder getVolume " + cylinder.getVolume());
         System.out.println("pyramid getVolume " + pyramid.getVolume());
 
-        ShapeList.sort(baseComp);
-        for (Shape s : ShapeList) {
+        for (Shape s : shapeList) {
+
+            System.out.println(s.getVolume() + "\t" + s.getBaseArea());
+
+        }
+
+        System.out.println("******************************************");
+
+        //shapeList.sort(baseComp);
+        //Collections.sort(shapeList,volumeComp);
+        for (Shape s : shapeList) {
 
             System.out.println(s.getVolume() + "\t" + s.getBaseArea());
         }
-        ShapeList.sort(volumeComp);
-        for (Shape s : ShapeList) {
+        System.out.println("******************************************");
+        //shapeList.sort(volumeComp);
+        //Collections.sort(shapeList,baseComp);
+        for (Shape s : shapeList) {
 
             System.out.println(s.getVolume() + "\t" + s.getBaseArea());
 
+        }
+
+        Object[] objArray = shapeList.toArray();
+        Shape[] shapeArray = Arrays.copyOf(objArray, objArray.length, Shape[].class);
+        Arrays.sort(shapeArray);
+        System.out.println("******************************************");
+        for (int i = 0; i < shapeArray.length; i++) {
+            System.out.println("\t" + shapeArray[i].getVolume() + " " + shapeArray[i].getBaseArea());
+        }
+
+        Sorting algorithm = new Sorting();
+        Debug debug = new Debug(1);
+        int unsortedArray[] = {35, 5, 1, 6, 12, 4, 9, 2, 66};
+
+        // algorithm.bubbleSort(unsortedArray, debug);
+        algorithm.quickSort(unsortedArray, debug);
+
+        for (int a : unsortedArray) {
+            System.out.print(a + " ");
         }
     }
 
