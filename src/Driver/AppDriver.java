3e10bd0f85765cc8105fa5ae998e2a11a00af364
Marcel Gallardo <116410849+MFGPerez@users.noreply.github.com>
2023-06-13 13:10:32 -0600

    added appDriver  ive been using
    args are implemented

--- src/Driver/AppDriver.java
+++ src/Driver/AppDriver.java
@@ -1,145 +1,241 @@
-package Driver;
+package driver;
 
-import Comparator.CompareByBaseArea;
-import Comparator.CompareByVolume;
-import Shape.Cone;
-import Shape.Cylinder;
-import Shape.Pyramid;
-import Shape.Shape;
-import Sorting.AlgorithmManager;
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-
+import shapes.Shape;
 import utilities.Debug;
+import utilities.FileReader;
+import utilities.SortingAlgorithms;
 
 /**
+ * Class description: Main class used to create FileReader, SortingAlgorithms,
+ * to sort a shape array
+ * 
+ * @author Marcel Gallardo (000827700)
  *
- *
+ * 
  */
 public class AppDriver {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-           //System.out.println(args[0]);
-        
-//        String filename;
-//        String sortType;
-//        String property;
-//        
-//        if(args.length <3)
-//            throw new RuntimeException("Few number of arguments");
-//        
-//        if (args[0].charAt(1) == 'f')
-//            filename = args[0].substring(2);
-//        else if (args[1].charAt(1) == 'f')
-//            filename = args[0].substring(2);
-//        else if (args[2].charAt(1) == 'f')
-//            filename = args[0].substring(2);
-//        
-//        
-//        if (args[0].charAt(1) == 'S')
-//            sortType = args[0].substring(2);
-//        else if (args[1].charAt(1) == 'S')
-//            sortType = args[0].substring(2);
-//        else if (args[2].charAt(1) == 'S')
-//            sortType = args[0].substring(2);
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
+		
+		/* ARGS /* 
+		/*
+		 * java -jar sort.jar -fpolyfor1.txt -Tv -Sb java -jar sort.jar -ta -sQ
+		 * -f"res\polyfor3".txt java -jar sort.jar -tH -F"C:\temp\polyfor5.txt" –sB
+		 * where v is volume, h is height, a is base area, b is bubble, s is selection,
+		 * i is insertion, m is merge, q is quick, and your choice of sorting algorithm
+		 * is z.
+		 */
 
-//        if(args[0].equalsIgnoreCase("merge")){
-//            algorithm.mergeSort(unsortedArray, debug);
-//        }
-            
-        //algorithm.mergeSort(unsortedArray, debug);
+		// args needed -F - file, B,S,I,M,Q - a sort , H,V,BA - what to compare by
+		// exaple of proper args -Fpolyfor1.txt -B -H
 
-        CompareByBaseArea baseComp = new CompareByBaseArea();
-        CompareByVolume volumeComp = new CompareByVolume();
+		
+		String filePath;
+		Debug debug;
+		Shape shapeArray[];
+		SortingAlgorithms algorithm;
+		FileReader reader;
 
-        Cone cone = new Cone(28437.601, 17222.616);
-        Cone cone1 = new Cone(328437.601, 317222.616);
-        Cylinder cylinder = new Cylinder(12595.147, 650.338);
-        Cylinder cylinder1 = new Cylinder(42595.147, 450.338);
-        Pyramid pyramid = new Pyramid(13081.024, 12232.336);
-        Pyramid pyramid1 = new Pyramid(53081.024, 52232.336);
+		
+		if (args.length < 1) {
+			throw new RuntimeException("Not Enough Arguments");
 
-        cone.calcBaseArea();
-        cone.calcVolume();
-        cylinder.calcBaseArea();
-        cylinder.calcVolume();
-        pyramid.calcBaseArea();
-        pyramid.calcVolume();
+		} else if (args[0].charAt(1) == 'F' && args[1].charAt(1) == 'B') {
 
-        System.out.println(cone.compareTo(cylinder));
+			//filePath = args[0].substring(2); - dosnt work for me on mac 
+		
+			// file path 
+			filePath = "/Users/mfgperez/Documents/University Classes/CMPRG 251 - OOP/Assignment1_311/res/shape_data/"+ args[0].substring(2);
+	
+			debug = new Debug(0); // debugger - set as level 1
+			
+			reader = new FileReader(filePath, debug); // reader passing file path and debugger 
 
-        ArrayList<Shape> shapeList = new ArrayList<>();
-        shapeList.add(cone);
-        shapeList.add(cylinder);
-        shapeList.add(pyramid);
-        shapeList.add(cone1);
-        shapeList.add(cylinder1);
-        shapeList.add(pyramid1);
+			shapeArray = reader.populateShapeArray(); // populate array 
 
-        System.out.println("cone base area " + cone.getBaseArea());
-        System.out.println("cylinder base area " + cylinder.getBaseArea());
-        System.out.println("pyramid base area " + pyramid.getBaseArea());
+			algorithm = new SortingAlgorithms(shapeArray); // pass array into the sorting algorithm constructor 
 
-        System.out.println("cone getVolume " + cone.getVolume());
-        System.out.println("cylinder getVolume " + cylinder.getVolume());
-        System.out.println("pyramid getVolume " + pyramid.getVolume());
+			algorithm.bubbleSort(debug); // call bubble sort 
 
-        for (Shape s : shapeList) {
+			/* Get shape arrays  height */ 
+			for (Shape a : shapeArray) {
+				System.out.println(a.getHeight() + " ");
+			}
+			System.out.println("-----------------------------");
+			System.out.println("array size " + shapeArray.length);
 
-            System.out.println(s.getVolume() + "\t" + s.getBaseArea());
+		} else if (args[0].charAt(1) == 'F' && args[1].charAt(1) == 'S') {
 
-        }
+			
+			
+			filePath = "/Users/mfgperez/Documents/University Classes/CMPRG 251 - OOP/Assignment1_311/res/shape_data/"+ args[0].substring(2);
+			debug = new Debug(0); // debugger - set as level 1
+			reader = new FileReader(filePath, debug);
 
-        System.out.println("******************************************");
+			shapeArray = reader.populateShapeArray();
 
-        //shapeList.sort(baseComp);
-        //Collections.sort(shapeList,volumeComp);
-        for (Shape s : shapeList) {
+			algorithm = new SortingAlgorithms(shapeArray);
 
-            System.out.println(s.getVolume() + "\t" + s.getBaseArea());
-        }
-        System.out.println("******************************************");
-        //shapeList.sort(volumeComp);
-        //Collections.sort(shapeList,baseComp);
-        for (Shape s : shapeList) {
+			algorithm.selectionSort(debug);
 
-            System.out.println(s.getVolume() + "\t" + s.getBaseArea());
+			for (Shape a : shapeArray) {
+				System.out.println(a.getHeight() + " ");
+			}
+			System.out.println("-----------------------------");
+			System.out.println("array size " + shapeArray.length);
 
-        }
+		} else if (args[0].charAt(1) == 'F' && args[1].charAt(1) == 'I') {
 
-        Object[] objArray = shapeList.toArray();
-        Shape[] shapeArray = Arrays.copyOf(objArray, objArray.length, Shape[].class);
-        
-        
-        Arrays.sort(shapeArray);
-        
-        
-        
-        System.out.println("******************************************");
-        for (int i = 0; i < shapeArray.length; i++) {
-            System.out.println("\t" + shapeArray[i].getVolume() + " " + shapeArray[i].getBaseArea());
-        }
+			filePath = "/Users/mfgperez/Documents/University Classes/CMPRG 251 - OOP/Assignment1_311/res/shape_data/"+ args[0].substring(2);
+			debug = new Debug(0); // debugger - set as level 1
+			reader = new FileReader(filePath, debug);
 
-        Debug debug = new Debug(1);
-        AlgorithmManager algorithm = new AlgorithmManager();
-        //int unsortedArray[] = {10,6,5,4,3,2};
+			shapeArray = reader.populateShapeArray();
 
-        algorithm.quickSort(shapeArray);
+			algorithm = new SortingAlgorithms(shapeArray);
 
-       
-        for (Shape s: shapeArray) {
-            System.out.print(s + " ");
-             System.out.println("  ");
-        }
-    }
+			algorithm.insertionSort(debug);
+
+			for (Shape a : shapeArray) {
+				System.out.println(a.getHeight() + " ");
+			}
+			System.out.println("-----------------------------");
+			System.out.println("array size " + shapeArray.length);
+
+		} else if (args[0].charAt(1) == 'F' && args[1].charAt(1) == 'M') {
+
+			filePath = "/Users/mfgperez/Documents/University Classes/CMPRG 251 - OOP/Assignment1_311/res/shape_data/"+ args[0].substring(2);
+			debug = new Debug(0); // debugger - set as level 1
+			reader = new FileReader(filePath, debug);
+
+			shapeArray = reader.populateShapeArray();
+
+			algorithm = new SortingAlgorithms(shapeArray);
+
+			algorithm.mergeSort(shapeArray, debug);
+
+			for (Shape a : shapeArray) {
+				System.out.println(a.getHeight() + " ");
+			}
+			System.out.println("-----------------------------");
+			System.out.println("array size " + shapeArray.length);
+
+		} else if (args[0].charAt(1) == 'F' && args[1].charAt(1) == 'Q') {
+
+			filePath = "/Users/mfgperez/Documents/University Classes/CMPRG 251 - OOP/Assignment1_311/res/shape_data/"+ args[0].substring(2);
+			debug = new Debug(0); // debugger - to set level
+			reader = new FileReader(filePath, debug);
+
+			shapeArray = reader.populateShapeArray();
+
+			algorithm = new SortingAlgorithms(shapeArray);
+
+			algorithm.quickSort(debug);
+
+			for (Shape a : shapeArray) {
+				System.out.println(a.getHeight() + " ");
+			}
+			System.out.println("-----------------------------");
+			System.out.println("array size " + shapeArray.length);
+		}
+		else if (args[0].charAt(1) == 'F' && args[1].charAt(1) == 'G') {
+
+			filePath = "/Users/mfgperez/Documents/University Classes/CMPRG 251 - OOP/Assignment1_311/res/shape_data/"+ args[0].substring(2);
+			debug = new Debug(0); // debugger - to set level
+			reader = new FileReader(filePath, debug);
+
+			shapeArray = reader.populateShapeArray();
+
+			algorithm = new SortingAlgorithms(shapeArray);
+
+			 algorithm.gnomeSort(debug);
+
+			for (Shape a : shapeArray) {
+				System.out.println(a.getHeight() + " ");
+			}
+			System.out.println("-----------------------------");
+			System.out.println("array size " + shapeArray.length);
+		}
+		
+
+	
+		//debug = new Debug(0); // debugger - set as level 1
+
+		//String path = "/Users/mfgperez/Documents/University Classes/CMPRG 251 - OOP/Assignment1_311/res/shape_data/polyfor1.txt";
+
+		// file reader object
+		 //reader = new FileReader(path, debug);
+
+		/* Shape Array (unsorted) and initialized */
+		 //shapeArray = reader.populateShapeArray();
+
+		// print out populated array and all shape names in array
+		// System.out.println("Populated array");
+		 
+		 
+		 //for (Shape a : shapeArray) {
+		 //System.out.println(a.getShapeName() + " ");
+		// }
+
+		/* SortingAlgorithms class constructor with a shape array */
+		 //algorithm = new SortingAlgorithms(shapeArray);
+
+		 /* Sorts  */
+		// algorithm.bubbleSort(debug); 
+		// algorithm.selectionSort(debug); 
+		// algorithm.quickSort(debug); 
+		// algorithm.insertionSort(debug); 
+		// algorithm.mergeSort(shapeArray); 
+		//  algorithm.gnomeSort(debug); - terrible takes to long 
+
+		// System.out.println("Sorted array based on hieght");
+
+		/* Display array (Sorted) */
+		 /*
+		 for (Shape a : shapeArray) {
+		   System.out.println(a.getHeight() + " ");
+		 }
+		 System.out.println("-----------------------------");
+		 System.out.println("array size " + shapeArray.length);
+          
+          
+          */
+		 
+		 
+		/* BASE AREA AND VOLUME TESTING */
+		/*
+		 * System.out.println("----------------------------------- ");
+		 * 
+		 * System.out.println("base area cone  = " + cone.calcArea()); // round these
+		 * off System.out.println("base cone volume  = " + cone.calcVolume());
+		 * 
+		 * System.out.println("----------------------------------- ");
+		 * 
+		 * Cylinder cylinder1 = new Cylinder("Cylinder", 50.60, 30.55);
+		 * System.out.println("base area cylinder = " + cylinder1.calcArea()); // round
+		 * these off System.out.println("base cylinder volume = " +
+		 * cylinder1.calcVolume());
+		 * 
+		 * System.out.println("----------------------------------- ");
+		 * 
+		 * System.out.println("base area pyramid = " + pyramid.calcArea()); // round
+		 * these off System.out.println("base pyramid volume = " +
+		 * pyramid.calcVolume());
+		 * 
+		 * System.out.println("----------------------------------- ");
+		 * 
+		 * OctagonalPrism octagonalPrism1 = new OctagonalPrism("OctagonalPrism", 50.24,
+		 * 60.55); System.out.println("base area octagon = " +
+		 * octagonalPrism1.calcArea()); // round these off
+		 * System.out.println("base ocatagon volume = " + octagonalPrism1.calcVolume());
+		 * 
+		 */
+
+		/* REFLECTION WOULD GO HEAR */
+	}
 
 }
